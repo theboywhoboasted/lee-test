1007f42561b66131e6dde14f15ab1553c56e1637
theboywhoboasted <kooldude.arpit@gmail.com>
2016-06-02 09:08:33 +0530

    Create lee-test.R

---
+++ lee-test.R
@@ -0,0 +1,67 @@
+library(systemfit)
+
+lee_one_break_test <- function(yt,lambda){
+  l <- length(yt)
+  tb <- floor(lambda*l)
+  df <- data.frame(yt)
+  df$one <- 1
+  df$time <- (1:l)
+  df$dt <- df$one
+  df$dtt <- df$time - tb
+  for (t in 1:tb){
+    df$dt[t] <- 0
+    df$dtt[t] <- 0
+  }
+  new_df <- data.frame(yt=diff(df$yt), dt=diff(df$dt), dtt=diff(df$dtt))
+  # new_df$one <- diff(df$one)
+  # print(new_df)
+  st <- residuals(lm(yt~dt+dtt,data=new_df))
+  reg_df <- new_df[2:(l-1),]
+  reg_df$st_1 <- st[1:(l-2)]
+  lin_mod <- lm(yt~dt+dtt+st_1,data=reg_df)
+  tval <- summary(lin_mod)$coefficients[,'t value']['st_1']
+  return(tval)
+}
+
+tvals <- numeric(500)
+times <- integer(500)
+for (i in 1:500){
+  d1 <- 1
+  d2 <- 2
+  d3 <- 3
+  d4 <- 4
+  phi <- 1
+  et <- rnorm(100)
+  xt <- cumsum(et)
+  l <- 100
+  tb <- 50
+  one <- 1
+  time <- (1:l)
+  dt <- one
+  dtt <- time - tb
+  for (t in 1:tb){
+    dt[t] <- 0
+    dtt[t] <- 0
+  }
+  yt <- d1*one + d2*time + d3*dt + d4*dtt + phi*xt
+  
+  min_t <- 1000
+  min_time <- 0
+  for (tb in 1:100){
+    t <- lee_one_break_test(yt,tb/100)
+    if (t<min_t){
+      min_t <- t
+      min_time <- tb
+    }
+  }
+  tvals[i] <- min_t
+  times[i] <- min_time
+  print(i)
+}
+
+lee_one_break_test(rnorm(1000),0.61)
+
+#   eq1 <- y ~ ylag + xlag + xl + yl
+#   eq2 <- x ~ ylag + xlag + xl + yl
+#   sol <- systemfit(formula=c(eq1,eq2))
+#   return(sol)
